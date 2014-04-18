a470712b22a73cb23cbde11e7b4f8f03799ff033
zhangxathust <zhanxnj@163.com>
2014-04-18 14:08:48 +0800

    二叉搜索树 使用泛型

--- java/Java Algorithm And DataStructure/src/search/BST.java
+++ java/Java Algorithm And DataStructure/src/search/BST.java
@@ -40,6 +40,44 @@ class Node<T> {
 
 }
 
+// a test Object
+class NodeValue implements Comparable<NodeValue> {
+
+	private int x;
+	private int y;
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public NodeValue() {
+	}
+
+	public NodeValue(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
+	@Override
+	public int compareTo(NodeValue o) {
+		// TODO Auto-generated method stub
+		if (this.x != o.getX()) {
+			return this.x - o.getX();
+		} else {
+			return this.y - o.getY();
+		}
+	}
+}
+
 public class BST {
 
 	// test main method
@@ -47,7 +85,7 @@ public class BST {
 		int[] array = { 19, 12, 3, 22, 6, 7, 21, 11, 43 };
 		Node<Integer> root = new Node<Integer>(array[0]);
 		for (int i = 1; i < array.length; i++) {
-			binarySort(root, array[i]);
+			binaryInsert(root, array[i]);
 		}
 
 		// look this binary tree
@@ -61,45 +99,51 @@ public class BST {
 	}
 
 	// 二叉排序树插入操作
-	public static void binarySort(Node<Integer> root, int key) {
-
-		int value = root.getValue();
-
-		if (key < value) {
+	// using fanxing hanshu
+	public static <T extends Comparable<T>> void binaryInsert(Node<T> root,
+			T key) {
+		if (root == null) {
+			return;
+		}
+		T value = root.getValue();
+		if (key.compareTo(value) < 0) {
 			if (root.getL_child() == null) {
-				Node<Integer> r = new Node<Integer>(key);
+				Node<T> r = new Node<T>(key);
 				root.setL_child(r);
 			} else {
-				binarySort(root.getL_child(), key);
+				binaryInsert(root.getL_child(), key);
 			}
 		} else {
 			if (root.getR_child() == null) {
-				Node<Integer> r = new Node<Integer>(key);
+				Node<T> r = new Node<T>(key);
 				root.setR_child(r);
 			} else {
-				binarySort(root.getR_child(), key);
+				binaryInsert(root.getR_child(), key);
 			}
 		}
 	}
 
-	public static boolean binarySerach(Node<Integer> root, int key) {
+	// 二叉树搜索
+	public static <T extends Comparable<T>> boolean binarySerach(Node<T> root,
+			T key) {
 		if (root == null) {
 			// throw new NullPointerException();
 			return false;
-		} else if (root.getValue() == key) {
+		} else if (root.getValue().compareTo(key) == 0) {
 			return true;
-		} else if (root.getValue() < key) {
+		} else if (root.getValue().compareTo(key) < 0) {
 			return binarySerach(root.getR_child(), key);
 		} else {
 			return binarySerach(root.getL_child(), key);
 		}
 	}
 
-	public static boolean delete(Node<Integer> root, int key) {
+	// 删除某个节点
+	public static <T extends Comparable<T>> boolean delete(Node<T> root, T key) {
 		if (root == null) {
 			return false;
 		}
-		if (root.getValue() == key) {
+		if (root.getValue().compareTo(key) == 0) {
 			if (root.getR_child() == null) {
 				root = root.getL_child();
 				return true;
@@ -107,47 +151,45 @@ public class BST {
 				root = root.getR_child();
 				return true;
 			} else {
-
 				root = root.getR_child();
 				return true;
 			}
-		} else if (root.getValue() > key) {
+		} else if (root.getValue().compareTo(key) > 0) {
 			return delete(root.getL_child(), key);
 		} else {
 			return delete(root.getR_child(), key);
 		}
 	}
-	
-	private static Node<Integer> getSuccessor(Node<Integer> delNode)
-    {
-        // 后继节点的父节点
-        Node<Integer> successorParent = delNode;
-        // 后继节点
-        Node<Integer> successor = delNode.getR_child();
-        //判断后继节点是否有左孩子
-        Node<Integer> current = successor.getL_child();
-        while (current != null) {
-            successorParent = successor;
-            successor = current;
-            current = current.getL_child();
-        }
-        //当该后继节点是属于包含左子节点的右节点的子节点
-        if (successor != delNode.getR_child())
-        {
-            successorParent.setL_child(successor.getR_child());
-            //连接被删除节点的右孩子
-            successor.setR_child(delNode.getR_child());
-        }
-        return successor;
-    }
 
-	// 中序遍历
-	public static void inOrderTraverse(Node<Integer> root) {
+	// non modify
+	private static Node<Integer> getSuccessor(Node<Integer> delNode) {
+		// 后继节点的父节点
+		Node<Integer> successorParent = delNode;
+		// 后继节点
+		Node<Integer> successor = delNode.getR_child();
+		// 判断后继节点是否有左孩子
+		Node<Integer> current = successor.getL_child();
+		while (current != null) {
+			successorParent = successor;
+			successor = current;
+			current = current.getL_child();
+		}
+		// 当该后继节点是属于包含左子节点的右节点的子节点
+		if (successor != delNode.getR_child()) {
+			successorParent.setL_child(successor.getR_child());
+			// 连接被删除节点的右孩子
+			successor.setR_child(delNode.getR_child());
+		}
+		return successor;
+	}
+
+	// 中序遍历,递归实现
+	public static <T> void inOrderTraverse(Node<T> root) {
 		if (root == null) {
 			return;
 		}
 		inOrderTraverse(root.getL_child());
-		System.out.println(root.getValue());
+		System.out.print(root.getValue()+"  ");
 		inOrderTraverse(root.getR_child());
 	}
 }
