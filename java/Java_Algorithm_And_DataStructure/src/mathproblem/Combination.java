d82195fa13e4fc8873899465e37c91c6ecc3346f
zhangxathust <zhanxnj@163.com>
2014-05-05 20:33:41 +0800

    组合.

---
+++ java/Java_Algorithm_And_DataStructure/src/mathproblem/Combination.java
@@ -0,0 +1,43 @@
+package mathproblem;
+
+import java.util.Stack;
+
+/**
+ * @author Administrator
+ * @see ./c++/Math/Combination.cpp
+ */
+public class Combination {
+	public static void main(String[] args) {
+		String s = "abc";
+		getAllCombination(s);
+	}
+
+	public static void combination(String s, int i, int num,
+			Stack<Character> result) {
+		if (s == null) {
+			return;
+		}
+		if (num == 0) {
+			System.out.println(result);
+			return;
+		}
+		if (i >= s.length()) {
+			return;
+		}
+		result.push(s.charAt(i));
+		combination(s, i + 1, num - 1, result);
+		result.pop();
+		combination(s, i + 1, num, result);
+	}
+	
+	public static void getAllCombination(String s){
+		if(s==null){
+			return;
+		}
+		Stack<Character> result = new Stack<>();
+		int length = s.length();
+		for(int i=1;i<=length;i++){
+			combination(s, 0, i, result);
+		}
+	}
+}
